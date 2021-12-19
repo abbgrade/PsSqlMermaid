9679f7c515b28374b306ac1c0e80bf60394c36a4
Steffen Kampmann <steffen.kampmann@gmail.com>
2021-12-19 18:48:18 +0100

    added documentation

--- Source/Public/New-Diagram.ps1
+++ Source/Public/New-Diagram.ps1
@@ -1,13 +1,33 @@
 function New-Diagram {
 
+    <#
+
+    .SYNOPSIS
+    Creates an ER diagram for a Dac model.
+
+    .DESCRIPTION
+    Uses PsDac to select the tables and foreign key constrains from a database model and creates a mermaid Entity Relationship diagrams.
+
+    .EXAMPLE
+
+    PS C:\> Import-DacModel WideWorldImporters.dacpac | New-SqlMermaidDiagram
+
+    <svg id="mermaid-1639919827409" width="100%" xmlns="http://www.w3.org/2000/svg" ...
+
+    #>
+
     [CmdletBinding()]
     param (
+
+        # Specifies the database model.
         [Parameter(
             ValueFromPipeline
         )]
         # [Microsoft.SqlServer.Dac.Model.TSqlModel]
         $DacModel,
 
+        # Specifies the separator between schema and table name.
+        [Parameter()]
         $SchemaSeparator = '-'
     )
 
