6b2848f61b63ee689b0851ac903a932e53dc8ecf
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-02-27 14:53:20 +0100

    update things

--- Tasks/Testdata.Tasks.ps1
+++ Tasks/Testdata.Tasks.ps1
@@ -11,21 +11,24 @@ task Testdata.DacPac.WWI.Clean -If { $SqlServerSamplesDirectory.Exists } -Jobs {
 
 task Testdata.DacPac.WWI.AddSolution -If { -Not $SqlServerSamplesDirectory.Exists } -Jobs {
     New-Item $SqlServerSamplesDirectory -ItemType Directory
+}, Testdata.DacPac.WWI.InitSolution
+
+task Testdata.DacPac.WWI.InitSolution -If { -Not ( Test-Path "$SqlServerSamplesDirectory\.git" ) } -Jobs {
     Push-Location $SqlServerSamplesDirectory
     exec { git init }
     exec { git remote add origin -f https://github.com/microsoft/sql-server-samples.git }
-    exec { git config core.sparseCheckout true }
-    Set-Content .git/info/sparse-checkout $WwiSsdtRelativePath
     Pop-Location
 }
 
 task Testdata.DacPac.WWI.CheckoutSolution -If { -Not $WwiSsdtDirectory.Exists } -Jobs Testdata.DacPac.WWI.AddSolution, {
     Push-Location $SqlServerSamplesDirectory
+    exec { git config core.sparseCheckout true }
+    Set-Content .git/info/sparse-checkout $WwiSsdtRelativePath
     exec { git pull --depth=1 origin master --verbose }
     Pop-Location
 }
 
-task Testdata.DacPac.WWI.Create -Jobs Testdata.DacPac.WWI.CheckoutSolution, {
+task Testdata.DacPac.WWI.Create -If { -Not $WideWorldImportersDacPac.Exists } -Jobs Testdata.DacPac.WWI.CheckoutSolution, {
     # # can be enabled if dotnet core build is public and working
     # exec { dotnet build "$SqlServerSamplesDirectory\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj" /p:NetCoreBuild=true }
 
