5e75388de2926bddcde78a8438a9864f937c12d1
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-02-27 12:40:28 +0100

    add pull testdata

--- Tasks/Testdata.Tasks.ps1
+++ Tasks/Testdata.Tasks.ps1
@@ -1,28 +1,37 @@
 
-task Testdata.DacPac.WWI.Clean -If ( Test-Path $PSScriptRoot\..\test\sql-server-samples ) -Jobs {
-    Remove-Item $PSScriptRoot\..\test\sql-server-samples -Recurse -Force
+[System.IO.DirectoryInfo] $SqlServerSamplesDirectory = "$PSScriptRoot\..\Test\sql-server-samples"
+[string] $WwiSsdtRelativePath = 'samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt'
+[System.IO.DirectoryInfo] $WwiSsdtDirectory = Join-Path $SqlServerSamplesDirectory $WwiSsdtRelativePath
+
+task Testdata.DacPac.WWI.Clean -If ( $SqlServerSamplesDirectory.Exists ) -Jobs {
+    Remove-Item $SqlServerSamplesDirectory -Recurse -Force
 }
 
-task Testdata.DacPac.WWI.CheckoutSolution -If ( -Not ( Test-Path $PSScriptRoot\..\test\sql-server-samples )) -Jobs {
-    $testdata = New-Item $PSScriptRoot\..\test\sql-server-samples -ItemType Directory
-    Push-Location $testdata
+task Testdata.DacPac.WWI.AddSolution -If ( -Not $SqlServerSamplesDirectory.Exists ) -Jobs {
+    New-Item $SqlServerSamplesDirectory -ItemType Directory
+    Push-Location $SqlServerSamplesDirectory
     git init
     git remote add origin -f https://github.com/microsoft/sql-server-samples.git
     git config core.sparseCheckout true
-    Set-Content .git/info/sparse-checkout samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt
+    Set-Content .git/info/sparse-checkout $WwiSsdtRelativePath
+    Pop-Location
+}
+
+task Testdata.DacPac.WWI.CheckoutSolution -If ( -Not $WwiSsdtDirectory.Exists ) -Jobs Testdata.DacPac.WWI.AddSolution, {
+    Push-Location $SqlServerSamplesDirectory
     git pull --depth=1 origin master
     Pop-Location
 }
 
 task Testdata.DacPac.WWI.Create -Jobs Testdata.DacPac.WWI.CheckoutSolution, {
     # # can be enabled if dotnet core build is public and working
-    # exec { dotnet build "$PsScriptRoot\..\test\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj" /p:NetCoreBuild=true }
+    # exec { dotnet build "$SqlServerSamplesDirectory\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj" /p:NetCoreBuild=true }
 
     Import-Module Invoke-MsBuild
 
-    Invoke-MsBuild "$PsScriptRoot\..\test\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj"
+    Invoke-MsBuild "$SqlServerSamplesDirectory\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\WideWorldImporters.sqlproj"
 
-    assert ( Test-Path "$PsScriptRoot\..\test\sql-server-samples\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac" )
+    assert ( Test-Path "$SqlServerSamplesDirectory\samples\databases\wide-world-importers\wwi-ssdt\wwi-ssdt\bin\Debug\WideWorldImporters.dacpac" )
 }
 
 task Testdata.Create -Jobs Testdata.DacPac.WWI.Create
