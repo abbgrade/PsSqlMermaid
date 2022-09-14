8ae98dd95937881b1eb2ebbc6f020b4a8e259105
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-09-14 21:15:09 +0200

    changed case

--- Test/multi-project-sample/customer/Customer.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [customer].[Customer]
-(
-	[Id] INT NOT NULL , 
-    [Name] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_Customer] PRIMARY KEY ([Id])
-)
--- Test/multi-project-sample/sales/Order.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [sales].[Order]
-(
-	[Id] INT NOT NULL , 
-    [CustomerId] INT NOT NULL, 
-    CONSTRAINT [PK_Order] PRIMARY KEY ([Id]), 
-    CONSTRAINT [FK_Order_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [customer].[Customer]([Id])
-)
