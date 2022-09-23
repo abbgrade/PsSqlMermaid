913aac6ad3e647bf6f4e1f871a8890dd2fab8c2d
Steffen <skampmann@munichre.com>
2022-09-23 13:36:00 +0200

    made testdata optional

--- tasks/Testdata.Tasks.ps1
+++ tasks/Testdata.Tasks.ps1
@@ -1,4 +1,9 @@
-. $PSScriptRoot/PsSqlTestTasks/SqlServerSamples.Tasks.ps1
-. $PSScriptRoot/PsSqlTestTasks/WideWorldImporters.Tasks.ps1
+if ( Test-Path $PSScriptRoot/PsSqlTestTasks/SqlServerSamples.Tasks.ps1 ) {
+    . $PSScriptRoot/PsSqlTestTasks/SqlServerSamples.Tasks.ps1
+}
+
+if ( Test-Path $PSScriptRoot/PsSqlTestTasks/WideWorldImporters.Tasks.ps1 ) {
+    . $PSScriptRoot/PsSqlTestTasks/WideWorldImporters.Tasks.ps1
+}
 
 task Testdata.Create -Jobs PsSqlTestTasks.WideWorldImporters.DacPac.Create
